c395208123b4eb54e4e13206bfe972f9b040e979
AnalectsOfConfucius <EssaysAndCriticism@gmail.com>
2016-12-30 13:56:26 +0800

    red yellow warning

--- app/service/filter.service.js
+++ app/service/filter.service.js
@@ -12,7 +12,7 @@ function loginFilter() {
 		if (red > 0 || yellow > 0) {
 			swal({
 	        title: "请注意，您已被红黄牌警告！",
-	        text: "你已收到红牌警告" + red + "张，收到黄牌预警" + yellow + "张。",
+	        text: "你已收到红牌" + red + "张，收到黄牌" + yellow + "张。",
 	        type: "warning",
 		    });
 		}
