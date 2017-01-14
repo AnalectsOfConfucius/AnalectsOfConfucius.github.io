3b59667dcc37b242183f65e01125847c3d0db6a6
AnalectsOfConfucius <EssaysAndCriticism@gmail.com>
2017-01-14 11:01:24 +0800

    update taskBoxList controller js

--- app/task/taskBoxList.controller.js
+++ app/task/taskBoxList.controller.js
@@ -1,52 +1,6 @@
 $(function () {
     initPage(0, 10);
-    $('.search-action').click(function () {
-        initQuery(0, 10);
-    });
 });
-
-function initQuery(page, size) {
-    var query = $('#query').val();
-    if (query == "" || query == null) {
-        initPage(0, 10);
-        return;
-    }
-    console.log(query);
-    var dataQuery = {
-        page: page,
-        size: size,
-        query: query,
-    };
-    $.ajax({
-        url: url,
-        type: 'GET',
-        // GET请求传递data
-        data: dataQuery,
-        async: true,
-        dataType: 'json',
-        success: function (data) {
-            if (data) {
-                console.log(data);
-                var tpl = [
-                    '{@each doubleRandomResults as it,index}',
-                    '<tr class="{@if it.task != null }read{@else}unread{@/if}">',
-                    '<td class="check-mail">',
-                    '</td>',
-                    '<td class="mail-ontact"><a href="taskDetail.html">${it.companyName}</a>',
-                    '</td>',
-                    '<td class="mail-subject"><a href="taskDetail.html">${it.task}</a>',
-                    '</td>',
-                    '<td class="">',
-                    '</td>',
-                    '<td class="text-right mail-date">${it.doubleRandom.doubleRandomDate}</td>',
-                    '</tr>',
-                    '{@/each}'].join('');
-                var html = juicer(tpl, data);
-                $('#tContent').html(html);
-            }
-        },
-    });
-};
 
 function initPage(page, size) {
     var url = window.apiPoint + 'double-random-results/login';
@@ -65,6 +19,8 @@ function initPage(page, size) {
         success: function (data) {
             if (data) {
                 console.log(data);
+                var result = {};
+                result['doubleRandomResults'] = data;
                 var tpl = [
                     '{@each doubleRandomResults as it,index}',
                     '<tr class="{@if it.id != 2 }read{@else}unread{@/if}">',
@@ -80,42 +36,9 @@ function initPage(page, size) {
                     '<td class="text-right mail-date">${it.doubleRandom.doubleRandomDate}</td>',
                     '</tr>',
                     '{@/each}'].join('');
-                var html = juicer(tpl, data);
+                var html = juicer(tpl, result);
                 $('#tContent').html(html);
             }
         },
     });
-};
-
-function deleteOne(id) {
-    swal({
-            title: "您确定要删除这条信息吗",
-            text: "删除后将无法恢复，请谨慎操作！",
-            type: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#DD6B55",
-            confirmButtonText: "是的，我要删除！",
-            cancelButtonText: "让我再考虑一下…",
-            closeOnConfirm: false,
-            closeOnCancel: false
-        },
-        function (isConfirm) {
-            if (isConfirm) {
-                $.ajax({
-                    url: window.apiPoint + 'task-projects/' + id,
-                    type: 'DELETE',
-                    async: true,
-                    dataType: 'json',
-                    complete: function (data) {
-                        console.log(data);
-                        if (data.status == 200 && data.statusText == "OK") {
-                            swal("删除成功！", "您已经永久删除了这条信息。", "success");
-                            initPage(0, 10);
-                        }
-                    },
-                });
-            } else {
-                swal("已取消", "您取消了删除操作！", "error");
-            }
-        });
 };
